9a45cac7bb03db7ada3a2d5a21d7faa660230548
jay-sukheja1 <jaysukheja7190@gmail.com>
2025-11-18 16:20:50 +0530

    Some changes

--- main.cpp
+++ main.cpp
@@ -1,16 +1,29 @@
-#include <iostream>
+#include<iostream>
+using namespace std;
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+class Hello {
+public:
+    //constructor
+    Hello() {
+        cout<<"I am constructor"<<endl;
     }
+    int data=0;
+    Hello(int val) {
+        data=val;
+        cout<<"Parameterized constructor is called"<<endl;
+    }
+    ~Hello() {
+        cout<<"I am Destructor"<<endl;
+    }
+};
 
+int main() {
+    Hello h;
+
+    Hello h1(67);
+    cout<<h1.data<<endl;
+
+    Hello &h2(h1);    // copy constructor
+    cout<<h2.data<<endl;
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
